2cfc267597996f8b9a0afce69e321c5557ffc82b
suu3 <ahob789@ewhain.net>
2023-06-19 01:57:28 +0900

    feat: card click 두번

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -1,20 +1,18 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import {
   card,
   image,
+  imageInfo,
   hoverImage,
+  clickImage,
   text,
   rectangle,
   circle,
 } from "./card.module.scss";
-import {
-  GatsbyImage,
-  IGatsbyImageData,
-  StaticImage,
-  getImage,
-} from "gatsby-plugin-image";
+import { GatsbyImage, IGatsbyImageData, getImage } from "gatsby-plugin-image";
 import classNames from "classnames";
-import { Link } from "gatsby";
+import { navigate } from "gatsby";
+import useIsMobile from "@/hooks/useIsMobile";
 
 type CardType = {
   item: {
@@ -27,30 +25,58 @@ type CardType = {
     project_image: IGatsbyImageData;
   };
 };
+
 const Card = ({ item: { uid, title, authors, project_image } }: CardType) => {
+  const isMobile = useIsMobile();
+  const [clickCnt, setClickCnt] = useState(0);
+
+  const goToDetail = useCallback(() => {
+    navigate(`/project/${uid}`);
+  }, []);
+
+  const handleClick = () => {
+    if (isMobile) {
+      setClickCnt((prev) => prev + 1);
+      if (clickCnt === 0) {
+        //한번 클릭
+      } else {
+        //두번 클릭
+        goToDetail();
+      }
+    } else {
+      goToDetail();
+    }
+  };
+
+  const hoverImageCls = {
+    [imageInfo]: true,
+    [hoverImage]: !isMobile,
+    [clickImage]: isMobile && clickCnt >= 1,
+  };
+
   return (
-    <Link to={`/project/${uid}`}>
-      <div className={card}>
-        <div className={image}>
-          <GatsbyImage image={getImage(project_image)} alt={title} />
-        </div>
-        <div className={classNames(hoverImage)}>
-          <div className={circle} />
-          <div className={rectangle} />
-          <div className={text}>
-            <p>{title}</p>
-            <p>
-              {authors.map(({ name }, idx) => (
-                <span key={idx}>
-                  {name}
-                  {idx !== authors.length - 1 && ", "}
-                </span>
-              ))}
-            </p>
-          </div>
+    // <Link to={`/project/${uid}`}>
+    <div className={card} onClick={handleClick}>
+      <div className={image}>
+        <GatsbyImage image={getImage(project_image)} alt={title} />
+      </div>
+      <div className={classNames(hoverImageCls)}>
+        <div className={circle} />
+        <div className={rectangle} />
+        <div className={text}>
+          <p>{title}</p>
+          <p>
+            {authors.map(({ name }, idx) => (
+              <span key={idx}>
+                {name}
+                {idx !== authors.length - 1 && ", "}
+              </span>
+            ))}
+          </p>
         </div>
       </div>
-    </Link>
+    </div>
+    // </Link>
   );
 };
 
