f15e7a873fce65912e219fac1ada64bdb1aec9ca
suu3 <ahob789@ewhain.net>
2023-06-26 01:32:56 +0900

    fix: 피그마 맞춤

---
+++ src/layouts/SEO/index.tsx
@@ -0,0 +1,14 @@
+import { HeadFC } from "gatsby";
+import React from "react";
+
+const SEO = () => (
+  <>
+    <meta
+      name="description"
+      content="제 40회 서울여자대학교 시각디자인전공 졸업전시회"
+    />
+    <meta name="keywords" content="시각디자인,서울여자대학교,졸업전시" />
+  </>
+);
+
+export default SEO;
