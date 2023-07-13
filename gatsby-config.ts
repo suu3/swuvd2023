cf5b82daf6275ad4b4f44968390cd5b96560871a
suu3 <sk.byun@muzlive.com>
2023-07-13 15:44:11 +0900

    fix: favicon ico -> svg

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -28,7 +28,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: `./src/images/favicon.ico`,
+        icon: `./src/images/favicon.svg`,
       },
     },
     {
