e8a4bb3d884a273cbaf5d8641cb45c62f66e40eb
suu3 <ahob789@ewhain.net>
2023-07-15 12:41:30 +0900

    fix: favicon 규격 수정

--- src/layouts/SEO/index.tsx
+++ src/layouts/SEO/index.tsx
@@ -6,7 +6,7 @@ const SEO = () => (
       name="description"
       content="제 40회 서울여자대학교 시각디자인전공 졸업전시회"
     />
-    <meta name="keywords" content="시각디자인,서울여자대학교,졸업전시" />
+    <meta name="keywords" content="시각디자인,서울여자대학교,졸업전시,금을넘어" />
   </>
 );
 
